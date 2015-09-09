3ff87d15767cf4a32ad8e01c145cc48ddb67e047
bgoubin <b.goubin.56@gmail.com>
2015-09-09 02:05:31 +0200

    description of the cards

--- class/gameType/Tock.js
+++ class/gameType/Tock.js
@@ -22,6 +22,22 @@ module.exports = function GameType(){
    };
 
 
+   var description = {
+      "1":  {"spawn":true,    "own":true,    "opp":false,   "decomp":false,   "smash":false, "values":[1,11],  "swap":false,  "target":1},
+      "2":  {"spawn":false,   "own":true,    "opp":false,   "decomp":false,   "smash":false, "values":[2],     "swap":false,  "target":1},
+      "3":  {"spawn":false,   "own":true,    "opp":false,   "decomp":false,   "smash":false, "values":[3],     "swap":false,  "target":1},
+      "4":  {"spawn":false,   "own":true,    "opp":false,   "decomp":false,   "smash":false, "values":[-4],    "swap":false,  "target":1},
+      "5":  {"spawn":false,   "own":false,   "opp":true,    "decomp":false,   "smash":false, "values":[5],     "swap":false,  "target":1},
+      "6":  {"spawn":false,   "own":true,    "opp":false,   "decomp":false,   "smash":false, "values":[6],     "swap":false,  "target":1},
+      "7":  {"spawn":false,   "own":true,    "opp":false,   "decomp":true,    "smash":false, "values":[7],     "swap":false,  "target":"x"},
+      "8":  {"spawn":false,   "own":true,    "opp":false,   "decomp":false,   "smash":false, "values":[8],     "swap":false,  "target":1},
+      "9":  {"spawn":false,   "own":true,    "opp":false,   "decomp":false,   "smash":false, "values":[9],     "swap":false,  "target":1},
+      "10": {"spawn":false,   "own":true,    "opp":false,   "decomp":false,   "smash":false, "values":[10],    "swap":false,  "target":1},
+      "J":  {"spawn":false,   "own":true,    "opp":true,    "decomp":false,   "smash":false, "values":[0],     "swap":true,   "target":2},
+      "Q":  {"spawn":false,   "own":true,    "opp":false,   "decomp":false,   "smash":false, "values":[12],    "swap":false,  "target":1},
+      "K":  {"spawn":true,    "own":true,    "opp":false,   "decomp":false,   "smash":true,  "values":[13],    "swap":false,  "target":1},
+   };
+
    this.play = function(card, player){};
 
 };
